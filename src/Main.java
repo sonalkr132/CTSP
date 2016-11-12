2b99366499e2863922189d265c0239a3b3e3600b
Aditya Prakash <aditya.prakash132@gmail.com>
2016-11-12 12:21:57 +0530

    Show output inline|

--- src/Main.java
+++ src/Main.java
@@ -36,65 +36,47 @@ public class Main {
     p.initialize_population();
     
     //Intial Chromosome score and genes
-//    System.out.println("\n\nIntial Chromosome score and genes:\n");
-//    for(int i = 0; i < POPULATION_SIZE; i++){
-//      ArrayList<Integer> genes = p.chromosomes[i].genes;
-//      System.out.print("[" + p.chromosomes[i].score + "] ");
-//      for(int j = 0; j < genes.size(); j++){
-//        System.out.print(genes.get(j) + " ");
-//      }
-//      System.out.println();
-//    }
-//    
-//    //Best Chromosome score and gene
-//    System.out.println("\n\nBest Chromosome score and genes:\n");
-//    ArrayList<Integer> genes = p.alltime_best_chromosome.genes;
-//    System.out.print("[" + p.alltime_best_chromosome.score + "] ");
-//    for(int j = 0; j < genes.size(); j++){
-//      System.out.print(genes.get(j) + " ");
-//    }
-//    System.out.println("\nNEXT GENERATION\n");
+    System.out.println("\n\nIntial Chromosome score and genes:\n");
+    for(int i = 0; i < POPULATION_SIZE; i++){
+      ArrayList<Integer> genes = p.chromosomes[i].genes;
+      System.out.print("[" + p.chromosomes[i].score + "] ");
+      for(int j = 0; j < genes.size(); j++){
+        System.out.print(genes.get(j) + " ");
+      }
+      System.out.println();
+    }
+    
+    //Best Chromosome score and gene
+    System.out.println("\n\nBest Chromosome score and genes:\n");
+    ArrayList<Integer> genes = p.alltime_best_chromosome.genes;
+    System.out.print("[" + p.alltime_best_chromosome.score + "] ");
+    for(int j = 0; j < genes.size(); j++){
+      System.out.print(genes.get(j) + " ");
+    }
+    System.out.println("\nNEXT GENERATION\n");
 
-
-    File file = new File("/home/addie/data.txt");
-    try{
-      // creates the file
-      file.createNewFile();
-      // creates a FileWriter Object
-      FileWriter writer = new FileWriter(file);   
-      for(int i = 0; i < NUM_OF_ITR; i++){
-        p.next_generation();
-        // Writes the content to the file
-        if(i < 100){
-          writer.write(String.valueOf(p.alltime_best_chromosome.score) + "\n");
-        } else if(i % 50 == 0) {
-          writer.write(String.valueOf(p.alltime_best_chromosome.score) + "\n");
+  
+    for(int i = 0; i < NUM_OF_ITR; i++){
+      p.next_generation();
+      if(i < 20){
+        //Best Chromosome score and gene
+        System.out.println("\n\nBest Chromosome score and genes:\n");
+        genes = p.alltime_best_chromosome.genes;
+        System.out.print("[" + p.alltime_best_chromosome.score + "] ");
+        for(int j = 0; j < genes.size(); j++){
+          System.out.print(genes.get(j) + " ");
         }
       }
-      
-      writer.flush();
-      writer.close();
-    } catch (Exception e) {
-      System.out.println(e);
+      else if(i % 100 == 0){
+        //Best Chromosome score and gene
+        System.out.println("\n\nBest Chromosome score and genes:\n");
+        genes = p.alltime_best_chromosome.genes;
+        System.out.print("[" + p.alltime_best_chromosome.score + "] ");
+        for(int j = 0; j < genes.size(); j++){
+          System.out.print(genes.get(j) + " ");
+        }
+      }
     }
   }
 }
 
-//if(i < 20){
-//  //Best Chromosome score and gene
-//  System.out.println("\n\nBest Chromosome score and genes:\n");
-//  ArrayList<Integer> genes = p.alltime_best_chromosome.genes;
-//  System.out.print("[" + p.alltime_best_chromosome.score + "] ");
-//  for(int j = 0; j < genes.size(); j++){
-//    System.out.print(genes.get(j) + " ");
-//  }
-//}
-//else if(i % 100 == 0){
-//  //Best Chromosome score and gene
-//  System.out.println("\n\nBest Chromosome score and genes:\n");
-//  ArrayList<Integer> genes = p.alltime_best_chromosome.genes;
-//  System.out.print("[" + p.alltime_best_chromosome.score + "] ");
-//  for(int j = 0; j < genes.size(); j++){
-//    System.out.print(genes.get(j) + " ");
-//  }
-//}
